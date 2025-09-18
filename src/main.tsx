0cdaf7cf307279d3f19f595d79f40907dfd9feff
FrançoisMans10 <francoismansare@gmail.com>
2025-09-18 05:38:22 +0200

    Switch to HashRouter for deep links

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // 👈 HashRouter ici
 import App from "./App";
 import AboutPage from "./pages/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage";
@@ -8,15 +9,14 @@ import ContactPage from "./pages/ContactPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/contact" element={<ContactPage />} />
-
         <Route path="*" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
