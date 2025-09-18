c3df19faaf0db86a13d2789fa998717db5dd325f
FrançoisMans10 <francoismansare@gmail.com>
2025-09-18 05:49:11 +0200

    Switch to Browser for deep links

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom"; // 👈 HashRouter ici
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import AboutPage from "./pages/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage";
@@ -9,14 +8,15 @@ import ContactPage from "./pages/ContactPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/contact" element={<ContactPage />} />
+
         <Route path="*" element={<App />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
